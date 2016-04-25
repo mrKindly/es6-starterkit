6fc0f54ec7266f1aee7243b1a30a44e850dda9ab
mrKindly <fedyaev1992@gmail.com>
2016-04-25 14:04:54 +0300

    sourcemaps is added

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,6 +2,7 @@
 
 import gulp from 'gulp';
 import babel from 'gulp-babel';
+import sourcemaps from 'gulp-sourcemaps';
 
 const dirs = {
 	src: 'src',
@@ -11,8 +12,10 @@ const dirs = {
 
 gulp.task('default',() => {
 	return gulp.src(`${dirs.src}/app.js`)
+			.pipe(sourcemaps.init())
 			.pipe(babel({
 				presets: ['es2015']
 			}))
+			.pipe(sourcemaps.write('.'))
 			.pipe(gulp.dest(`${dirs.dest}`));
 });
